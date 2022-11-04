b1e37f8aad45f1887f014b68242a8e519e52f94f
harlit <harlit.estevez@gmail.com>
2022-11-04 11:33:29 -0400

    se agrego model de vacaciones y el contexto

---
+++ Models/Vacaciones.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Vacaciones
+{
+    [Key]
+    public int VacacionesId { get; set; }
+    [Required(ErrorMessage = "Favor de Ingresar el Incentivo.")]
+    public int Incentivo { get; set; }
+    [Required(ErrorMessage = "Favor de Ingresar el dia de inicio.")]
+    public DateTime DiasInicio { get; set; }
+    [Required(ErrorMessage = "Favor de Ingresar el dia de fin.")]
+    public DateTime DiasFinal { get; set; }
+
+    [ForeignKey("EmpleadoId")]
+    public int EmpleadoId { get; set; }
+}
